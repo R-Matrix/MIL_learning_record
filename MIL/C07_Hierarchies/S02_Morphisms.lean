7fe9742a3cd02957e5d1dae7664ae6f4a1d1de6a
R_Matrix <dsq1961248553@sjtu.edu.cn>
2025-08-16 00:53:35 +0800

    C07_S02_Morphisms --- 2025/08/16

--- MIL/C07_Hierarchies/S02_Morphisms.lean
+++ MIL/C07_Hierarchies/S02_Morphisms.lean
@@ -6,7 +6,7 @@ set_option autoImplicit true
 
 def isMonoidHom₁ [Monoid G] [Monoid H] (f : G → H) : Prop :=
   f 1 = 1 ∧ ∀ g g', f (g * g') = f g * f g'
-structure isMonoidHom₂ [Monoid G] [Monoid H] (f : G → H) : Prop where
+structure isMonoidHom₂[Monoid G] [Monoid H] (f : G → H) : Prop where
   map_one : f 1 = 1
   map_mul : ∀ g g', f (g * g') = f g * f g'
 example : Continuous (id : ℝ → ℝ) := continuous_id
@@ -45,10 +45,20 @@ class MonoidHomClass₁ (F : Type) (M N : Type) [Monoid M] [Monoid N] where
   map_one : ∀ f : F, toFun f 1 = 1
   map_mul : ∀ f g g', toFun f (g * g') = toFun f g * toFun f g'
 
-
 def badInst [Monoid M] [Monoid N] [MonoidHomClass₁ F M N] : CoeFun F (fun _ ↦ M → N) where
   coe := MonoidHomClass₁.toFun
 
+-- set_option synthInstance.checkSynthOrder false in
+-- instance [Monoid M] [Monoid N] [MonoidHomClass₁ F M N] : CoeFun F (fun _ ↦ M → N) where
+--   coe := MonoidHomClass₁.toFun
+/-
+将此设为实例是不好的。当面对类似 f x 的情况，而 f 的类型不是函数类型时，Lean 会尝试查找一个 CoeFun 实例来将 f 转换为函数。上述函数的类型为：
+{M N F : Type} → [Monoid M] → [Monoid N] → [MonoidHomClass₁ F M N] → CoeFun F (fun x ↦ M → N)
+
+因此，在尝试应用它时，Lean 无法预先确定未知类型 M 、 N 和 F 应该以何种顺序进行推断。这与我们之前看到的情况略有不同，但归根结底是同一个问题：
+在不知道 M 的情况下，Lean 将不得不在未知类型上搜索单子实例，从而无望地尝试数据库中的每一个单子实例。
+如果您想看看这种实例的效果，可以在上述声明的顶部输入 set_option synthInstance.checkSynthOrder false in ，将 def badInst 替换为 instance ，然后在这个文件中查找随机出现的错误。
+-/
 
 class MonoidHomClass₂ (F : Type) (M N : outParam Type) [Monoid M] [Monoid N] where
   toFun : F → M → N
@@ -105,13 +115,25 @@ structure OrderPresHom (α β : Type) [LE α] [LE β] where
 structure OrderPresMonoidHom (M N : Type) [Monoid M] [LE M] [Monoid N] [LE N] extends
 MonoidHom₁ M N, OrderPresHom M N
 
-class OrderPresHomClass (F : Type) (α β : outParam Type) [LE α] [LE β]
+class OrderPresHomClass (F : Type) (α β : outParam Type) [LE α] [LE β] extends
+  DFunLike F α (fun _ ↦ β) where
+  le_of_le : ∀ (f : F) (a a' : α), a ≤ a' → f a ≤ f a'
 
 instance (α β : Type) [LE α] [LE β] : OrderPresHomClass (OrderPresHom α β) α β where
+  coe := OrderPresHom.toFun
+  coe_injective' _ _ := OrderPresHom.ext
+  le_of_le := OrderPresHom.le_of_le
 
 instance (α β : Type) [LE α] [Monoid α] [LE β] [Monoid β] :
     OrderPresHomClass (OrderPresMonoidHom α β) α β where
+  coe := fun f ↦ f.toOrderPresHom.toFun
+  coe_injective' := fun _ _↦ OrderPresMonoidHom.ext
+  le_of_le := fun f ↦ f.toOrderPresHom.le_of_le
 
 instance (α β : Type) [LE α] [Monoid α] [LE β] [Monoid β] :
     MonoidHomClass₃ (OrderPresMonoidHom α β) α β
-  := sorry
+  where
+    coe := fun f ↦ f.toMonoidHom₁.toFun
+    coe_injective' := fun _ _ ↦ OrderPresMonoidHom.ext
+    map_one := fun f ↦ f.toMonoidHom₁.map_one
+    map_mul := fun f ↦ f.toMonoidHom₁.map_mul
